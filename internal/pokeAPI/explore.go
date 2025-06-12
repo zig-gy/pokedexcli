1eb295096c3c60287b88173d7fdd3d53e3c251df
zig-gy <benjamin.f.chadwick@proton.me>
2025-06-12 09:41:01 -0400

    cleaned up the LocationInfo struct

--- internal/pokeAPI/explore.go
+++ internal/pokeAPI/explore.go
@@ -36,54 +36,14 @@ func FetchPokemon(url string, cache *pokecache.Cache) ([]string, error) {
 }
 
 type LocationInfo struct {
-	EncounterMethodRates []struct {
-		EncounterMethod struct {
-			Name string `json:"name,omitempty"`
-			URL  string `json:"url,omitempty"`
-		} `json:"encounter_method,omitempty"`
-		VersionDetails []struct {
-			Rate    int `json:"rate,omitempty"`
-			Version struct {
-				Name string `json:"name,omitempty"`
-				URL  string `json:"url,omitempty"`
-			} `json:"version,omitempty"`
-		} `json:"version_details,omitempty"`
-	} `json:"encounter_method_rates,omitempty"`
-	GameIndex int `json:"game_index,omitempty"`
-	ID        int `json:"id,omitempty"`
-	Location  struct {
-		Name string `json:"name,omitempty"`
-		URL  string `json:"url,omitempty"`
-	} `json:"location,omitempty"`
-	Name  string `json:"name,omitempty"`
-	Names []struct {
-		Language struct {
-			Name string `json:"name,omitempty"`
-			URL  string `json:"url,omitempty"`
-		} `json:"language,omitempty"`
-		Name string `json:"name,omitempty"`
-	} `json:"names,omitempty"`
-	PokemonEncounters []struct {
-		Pokemon struct {
-			Name string `json:"name,omitempty"`
-			URL  string `json:"url,omitempty"`
-		} `json:"pokemon,omitempty"`
-		VersionDetails []struct {
-			EncounterDetails []struct {
-				Chance          int   `json:"chance,omitempty"`
-				ConditionValues []any `json:"condition_values,omitempty"`
-				MaxLevel        int   `json:"max_level,omitempty"`
-				Method          struct {
-					Name string `json:"name,omitempty"`
-					URL  string `json:"url,omitempty"`
-				} `json:"method,omitempty"`
-				MinLevel int `json:"min_level,omitempty"`
-			} `json:"encounter_details,omitempty"`
-			MaxChance int `json:"max_chance,omitempty"`
-			Version   struct {
-				Name string `json:"name,omitempty"`
-				URL  string `json:"url,omitempty"`
-			} `json:"version,omitempty"`
-		} `json:"version_details,omitempty"`
-	} `json:"pokemon_encounters,omitempty"`
+    PokemonEncounters []PokemonEncounter `json:"pokemon_encounters"`
+}
+
+type PokemonEncounter struct {
+    Pokemon Pokemon `json:"pokemon"`
+}
+
+type Pokemon struct {
+    Name string `json:"name"`
+    URL  string `json:"url"`
 }
